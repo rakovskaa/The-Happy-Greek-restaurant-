c9a0e610f319fbc2ddbfce7838b2f3ac751029cf
rakovskaa <rakovskaa@yahoo.com>
2025-08-15 19:08:04 -0400

    Update restaurant-menu.js

--- restaurant-menu.js
+++ restaurant-menu.js
@@ -1,4 +1,5 @@
 
+  
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
   import { getFirestore, collection, addDoc, getDocs, query, orderBy } 
